aca782ae4e0d019d4213c68264a0f88d64562291
tiashDev <rbmtiash@gmail.com>
2025-05-02 22:37:18 +0600

    Update index.js

--- morsel/index.js
+++ morsel/index.js
@@ -208,7 +208,7 @@ syntaxHighlight({ elmnt, mode, theme = morsel.defaultTheme }) {
     var s, e, d = "", a = [];
     while (str.search(start) > -1) {
       s = str.search(start);
-      e = str.indexOf(end, s+1);
+      e = str.indexOf(end, s+2);
       if (e == -1) {e = str.length;}
       if (repl) {
         a.push(func(str.substring(s, e + (end.length))));      
